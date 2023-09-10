19e692f473725aab25c9cacb918db48f271890f5
Gokturk Acar <gokturk.acr2002@gmail.com>
2023-09-11 02:58:49 +0300

    Only the user who created the thread can send a message to thread Other users can view thread. Thus, maximum efficiency can be achieved

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,6 +5,23 @@ module.exports = {
     async execute(message) {
         if (message.author.bot) return
 
+        // 11 is the type of the thread channel. source = https://discord-api-types.dev/api/next/discord-api-types-v10/enum/ChannelType
+        if (message.channel.type === 11) {
+            console.log('threde atıldı')
+            const splitedName = message.channel.name.split(' ')
+
+            const threadCreatedUserId = splitedName[splitedName.length - 1]
+
+            if (threadCreatedUserId !== message.author.id) {
+                console.log('aynı değil')
+                message.delete()
+            } else {
+                message.channel.send(
+                    `bu threade sadece sen(${message.author}) yazabilirsin, burası senin threadin ${message.channel.id} ve şimdi şu yazına karşılık olarak bu thredde yazıyorum "${message.content}"`
+                )
+            }
+        }
+
         if (message.channel.name === 'taiga') {
             console.log('sent message to taiga')
             const threadOptions = {
