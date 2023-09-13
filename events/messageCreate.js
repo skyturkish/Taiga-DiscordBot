8daf3b8afe45e6433d0da0e16cc65b9f6f37f31b
Gokturk Acar <gokturk.acr2002@gmail.com>
2023-09-13 20:37:43 +0300

    Thread management with Taiga
    If someone on the server mentions Taiga on any channel.
    Taiga creates a thread, in this created thread,
    anyone can write and talk to taiga.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,37 +5,35 @@ module.exports = {
     async execute(message) {
         if (message.author.bot) return
 
-        // 11 is the type of the thread channel. source = https://discord-api-types.dev/api/next/discord-api-types-v10/enum/ChannelType
-        if (message.channel.type === 11) {
-            console.log('threde atıldı')
-            const splitedName = message.channel.name.split(' ')
+        if (message.mentions.users.has('1149799424000794655')) {
+            const actualContent = message.content
+                .replace(/<@[^>]*>/g, '')
+                .trim()
 
-            const threadCreatedUserId = splitedName[splitedName.length - 1]
-
-            if (threadCreatedUserId !== message.author.id) {
-                console.log('aynı değil')
-                message.delete()
+            if (actualContent.length < 4) {
+                message.channel.send('Please write more than 4 characters')
             } else {
-                message.channel.send(
-                    `bu threade sadece sen(${message.author}) yazabilirsin, burası senin threadin ${message.channel.id} ve şimdi şu yazına karşılık olarak bu thredde yazıyorum "${message.content}"`
+                const threadName = actualContent.substring(0, 100)
+
+                const threadOptions = {
+                    name: threadName,
+                    autoArchiveDuration: 60,
+                }
+
+                const thread = await message.startThread(threadOptions)
+
+                thread.send(
+                    `This message is sent by Taiga for ${message.author.username} and it is created as a response to : ${actualContent}`
                 )
             }
         }
-
-        if (message.channel.name === 'taiga') {
-            console.log('sent message to taiga')
-            const threadOptions = {
-                name:
-                    message.content.substring(0, 50) +
-                    ' asked by ' +
-                    message.author.id,
-                autoArchiveDuration: 60,
-            }
-
-            const thread = await message.startThread(threadOptions)
-
-            thread.send(
-                `This message is sent by ${message.author.username} and it is created as a response to : ${message.content}`
+        // 11 is the type of the thread channel. source = https://discord-api-types.dev/api/next/discord-api-types-v10/enum/ChannelType
+        if (
+            message.channel.type === 11 &&
+            message.channel.ownerId === '1149799424000794655'
+        ) {
+            message.channel.send(
+                `bu threade sadece sen(${message.author}) yazabilirsin, burası senin threadin ${message.channel.id} ve şimdi şu yazına karşılık olarak bu thredde yazıyorum "${message.content}"`
             )
         }
     },
